e409a1dd4576a22aceed6b6c33c2a3bc04865a75
Dominik Vogel <xpossseidon@gmail.com>
2017-04-06 18:44:52 +0200

    a lot of constants and public readonly properties for memory access

--- Source/Main.pas
+++ Source/Main.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, SynEdit, Forms, Controls, Graphics, Dialogs, ExtCtrls, Menus, ActnList, StdCtrls, Grids,
-  ComCtrls, ValEdit;
+  ComCtrls, ValEdit, ProcessorDefine;
 
 type
 
@@ -12,16 +12,27 @@ type
 
   TfrmMain = class(TForm)
     actExit: TAction;
+    actFlashMem: TAction;
+    actCompile: TAction;
+    actStartStop: TAction;
     actRefreshMemory: TAction;
     actToggleMemoryVisible: TAction;
     actOpenFile: TAction;
     alActionList: TActionList;
+    btnCompile: TButton;
+    btnOpen: TButton;
+    btnStartStop: TButton;
     btnRefreshMemory: TButton;
+    btnFlash: TButton;
     cbMemorySelection: TComboBox;
     cbAutoRefreshMemory: TCheckBox;
     gbMemory: TGroupBox;
     gbPorts: TGroupBox;
     gbPeripherals: TGroupBox;
+    lbRuntime: TLabel;
+    lbCycles: TLabel;
+    lbRuntimeTitle: TLabel;
+    lbCyclesTitle: TLabel;
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
@@ -31,32 +42,52 @@ type
     miPlaceholder: TMenuItem;
     miFile: TMenuItem;
     mmMainMenu: TMainMenu;
+    Panel1: TPanel;
+    pnlInfo: TPanel;
+    pnlRuntime: TPanel;
     pnlMemorySelection: TPanel;
     pnlCenter: TPanel;
     pmMain: TPopupMenu;
-    Splitter1: TSplitter;
-    Splitter2: TSplitter;
+    pnlCycles: TPanel;
+    spltLeft: TSplitter;
+    spltRight: TSplitter;
+    Splitter3: TSplitter;
     StatusBar1: TStatusBar;
     edtEditor: TSynEdit;
     sgMemView: TStringGrid;
     procedure actExitExecute(Sender: TObject);
+    procedure actOpenFileExecute(Sender: TObject);
+    procedure actRefreshMemoryExecute(Sender: TObject);
     procedure actToggleMemoryVisibleExecute(Sender: TObject);
     procedure FormCloseQuery(Sender: TObject; var CanClose: boolean);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure FormDropFiles(Sender: TObject; const FileNames: array of String);
-    procedure vlMemoryValidateEntry(sender: TObject; aCol, aRow: Integer; const OldValue: string; var NewValue: String);
   private
+    FStartTime: Int64;
+    FCycles: Cardinal;
+    FProcessor: TProcessor;
+    FFileData: TStringList;
+
     function GetAutoRefreshMemory: Boolean;
     procedure ParseStringlist(List: TStringList);
+
+    function GetRuntime: Int64;
+    procedure SetCycles(AValue: Cardinal);
+    procedure SetStartTime(AValue: Int64);
+
 
     function GetMemoryVisible: Boolean;
     procedure SetAutoRefreshMemory(AValue: Boolean);
     procedure SetMemoryVisible(AValue: Boolean);
 
-  published
     property MemoryVisible: Boolean read GetMemoryVisible write SetMemoryVisible default True;
     property AutoRefreshMemory: Boolean read GetAutoRefreshMemory write SetAutoRefreshMemory default False;
+
+    procedure UpdateRuntime;
+    property StartTime: Int64 read FStartTime write SetStartTime;
+    property Runtime: Int64 read GetRuntime;
+    property Cycles: Cardinal read FCycles write SetCycles;
 
   end;
 
@@ -70,10 +101,11 @@ implementation
 { TfrmMain }
 
 procedure TfrmMain.FormCreate(Sender: TObject);
-var
-  I: Integer;
 begin
- { vlMemory.BeginUpdate;
+  FProcessor := TProcessor.Create;
+  FFileData := TStringList.Create;
+
+  { vlMemory.BeginUpdate;
   for I := 0 to $3FF do
   begin
     vlMemory.InsertRow(Format('0x%.4x', [I * 8]), 'DE AD BE EF 00 11 22 33', True);
@@ -84,6 +116,24 @@ end;
 procedure TfrmMain.actExitExecute(Sender: TObject);
 begin
   Close;
+end;
+
+procedure TfrmMain.actOpenFileExecute(Sender: TObject);
+begin
+  with TOpenDialog.Create(Self) do
+  begin
+    Filter := 'Compiled Program|*.lst|Binary Program|*.hex';
+    if Execute then
+    begin
+      FFileData.LoadFromFile(FileName);
+    end;
+    Free;
+  end;
+end;
+
+procedure TfrmMain.actRefreshMemoryExecute(Sender: TObject);
+begin
+
 end;
 
 procedure TfrmMain.actToggleMemoryVisibleExecute(Sender: TObject);
@@ -99,7 +149,8 @@ end;
 
 procedure TfrmMain.FormDestroy(Sender: TObject);
 begin
-
+  FProcessor.Free;
+  FFileData.Free;
 end;
 
 procedure TfrmMain.FormDropFiles(Sender: TObject; const FileNames: array of String);
@@ -107,10 +158,23 @@ begin
   // TODO: Load dropped file
 end;
 
-procedure TfrmMain.vlMemoryValidateEntry(sender: TObject; aCol, aRow: Integer; const OldValue: string;
-  var NewValue: String);
+function TfrmMain.GetRuntime: Int64;
 begin
-  NewValue := OldValue;
+  Result := GetTickCount64 - StartTime;
+end;
+
+procedure TfrmMain.SetCycles(AValue: Cardinal);
+begin
+  if FCycles = AValue then
+    Exit;
+  FCycles := AValue;
+  lbCycles.Caption := Format('%d', [AValue]);
+end;
+
+procedure TfrmMain.SetStartTime(AValue: Int64);
+begin
+  if FStartTime = AValue then Exit;
+  FStartTime := AValue;
 end;
 
 function TfrmMain.GetAutoRefreshMemory: Boolean;
@@ -131,15 +195,24 @@ begin
 end;
 
 procedure TfrmMain.SetMemoryVisible(AValue: Boolean);
+var
+  W: Integer;
 begin
   if MemoryVisible = AValue then
     Exit;
   gbMemory.Visible := AValue;
-  actToggleMemoryVisible.Checked := MemoryVisible;
+  W := gbMemory.BorderSpacing.ControlWidth + spltRight.BorderSpacing.ControlWidth;
   if MemoryVisible then
-    frmMain.Width := frmMain.Width + gbMemory.BorderSpacing.ControlWidth
+    frmMain.Width := frmMain.Width + W
   else
-    frmMain.Width := frmMain.Width - gbMemory.BorderSpacing.ControlWidth;
+    frmMain.Width := frmMain.Width - W;
+  spltRight.Visible := MemoryVisible;
+  actToggleMemoryVisible.Checked := MemoryVisible;
+end;
+
+procedure TfrmMain.UpdateRuntime;
+begin
+  lbRuntime.Caption := Format('%d ms', [Runtime]);
 end;
 
 procedure TfrmMain.ParseStringlist(List: TStringList);
--- Source/ProcessorDefine.pas
+++ Source/ProcessorDefine.pas
@@ -4,84 +4,218 @@ unit ProcessorDefine;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, windows, SysUtils;
 
 type
-
-  TProgramConter = Word;
-
-  TBank0 = (
-    b0IndirectAddr,
-    b0TMR0,
-    b0PCL,
-    b0STATUS,
-    b0FSR,
-    b0PORTA,
-    b0PORTB,
-    b0Unused,
-    b0EEDATA,
-    b0EEADR,
-    b0PCLATH,
-    b0INTCON
-  );
-
-  TBank1 = (
-    b1IndirectAddr = $80,
-    b1OPTION_REG,
-    b1PCL,
-    b1STATUS,
-    b1FSR,
-    b1TRISA,
-    b1TRISB,
-    b1Unused,
-    b1EECON1,
-    b1EECON2,
-    b1PCLATH,
-    b1INTCON
-  );
 
   { TProcessor }
 
   TProcessor = class
+  public const
+    // Stack
+    StackSize = 8;
+    // Program Memory
+    ProgramMemorySize = $400;
+    // Ram
+    RegisterBankOffset = $80;
+    RAMStart = $0C;
+    RAMEnd = $4F;
+    RAMSize = RAMEnd - RAMStart + 1;
+    RAMMappedStart = RAMStart + RegisterBankOffset;
+    RAMMappedEnd = RAMEnd + RegisterBankOffset;
+    // EEPRom
+    ROMSize = $40;
+
+  public type
+    TRegisterBank0 = (
+      b0IndirectAddr,
+      b0TMR0,
+      b0PCL,
+      b0STATUS,
+      b0FSR,
+      b0PORTA,
+      b0PORTB,
+      b0Unused,
+      b0EEDATA,
+      b0EEADR,
+      b0PCLATH,
+      b0INTCON
+    );
+
+    TRegisterBank1 = (
+      b1IndirectAddr = $80,
+      b1OPTION_REG,
+      b1PCL,
+      b1STATUS,
+      b1FSR,
+      b1TRISA,
+      b1TRISB,
+      b1Unused,
+      b1EECON1,
+      b1EECON2,
+      b1PCLATH,
+      b1INTCON
+    );
+
+    TProgramCounter = $0 .. $1FFF;
+    TProgramCounterStackPos = 0 .. StackSize - 1;
+    TInstruction = $0 .. $3FFF;
+    TProgramMemPos = 0 .. ProgramMemorySize - 1;
+    TProgramMemPointer = 0 .. ProgramMemorySize * 2 - 1;
+    TRAMPointer = $0 .. $FF;
+    TROMPointer = 0 .. ROMSize - 1;
+
+  public const
+    RegisterBank0Mapped: set of TRegisterBank0 = [
+      b0IndirectAddr,
+      b0PCL,
+      b0STATUS,
+      b0FSR,
+      b0PCLATH,
+      b0INTCON
+    ];
+
+    RegisterBank1Mapped: set of TRegisterBank1 = [
+      b1IndirectAddr,
+      b1PCL,
+      b1STATUS,
+      b1FSR,
+      b1PCLATH,
+      b1INTCON
+    ];
+
   private type
-    TInstruction = record
-      Command: Word;
+    TLineInstruction = record
+      Instruction: TInstruction;
       Line: Cardinal;
     end;
+
   private
-    FProgramCounter: TProgramConter;
-    FProgramCounterStack: array [0 .. 7] of TProgramConter;
-    FProgamMemory: array [0 .. $3FF] of TInstruction;
-    FRam: array [$0C .. $4F] of Byte;
-    FRegisterBank0: array [TBank0] of Byte;
-    FRegisterBank1: array [TBank1] of Byte;
-    FDataMemory: array [0 .. 63] of Byte;
+    // Program
+    FProgramCounter: TProgramCounter;
+    FProgramMem: array [TProgramMemPos] of TLineInstruction;
+
+    // Program Stack
+    FProgramCounterStackPos: TProgramCounterStackPos;
+    FProgramCounterStack: array [TProgramCounterStackPos] of TProgramCounter;
+
+    // Banks + RAM
+    FRAM: array [TRAMPointer] of Byte;
+
+    // EEPROM
+    FROM: array [TROMPointer] of Byte;
+    function GetDataMem(P: TROMPointer): Byte;
+    function GetProgramMem(P: TProgramMemPointer): Byte;
+    function GetRAM(P: TRAMPointer): Byte;
+    function GetRegisterBank0(S: TRegisterBank0): Byte;
+    function GetRegisterBank1(S: TRegisterBank1): Byte;
+
+    function NormalizeRAMPointer(ARAMPointer: TRAMPointer): TRAMPointer;
+    function CanNormalize(ARAMPointer: TRAMPointer): Boolean;
+
   public
-    procedure FlashProgramMemory(AFileData:TStringList);
+    procedure LoadProgram(AFileData:TStrings);
+    procedure Initialize;
+    procedure ResetROM;
+
+    procedure DoStep;
+
+    property ProgramMem[P: TProgramMemPointer]: Byte read GetProgramMem;
+    property RegisterBank0[S: TRegisterBank0]: Byte read GetRegisterBank0;
+    property RegisterBank1[S: TRegisterBank1]: Byte read GetRegisterBank1;
+    property RAM[P: TRAMPointer]: Byte read GetRAM;
+    property ROM[P: TROMPointer]: Byte read GetDataMem;
 
   end;
-
-
 
 implementation
 
 { TProcessor }
 
-procedure TProcessor.FlashProgramMemory(AFileData: TStringList);
+function TProcessor.GetDataMem(P: TROMPointer): Byte;
+begin
+  Result := FROM[P];
+end;
+
+function TProcessor.GetProgramMem(P: TProgramMemPointer): Byte;
+begin
+  if P mod 2 = 0 then
+    Result := (FProgramMem[P div 2].Instruction shr 8) and $FF
+  else
+    Result := FProgramMem[P div 2].Instruction and $FF;
+end;
+
+function TProcessor.GetRAM(P: TRAMPointer): Byte;
+begin
+  Result := FRAM[NormalizeRAMPointer(P)];
+end;
+
+function TProcessor.GetRegisterBank0(S: TRegisterBank0): Byte;
+begin
+  Result := FRAM[NormalizeRAMPointer(TRAMPointer(S))];
+end;
+
+function TProcessor.GetRegisterBank1(S: TRegisterBank1): Byte;
+begin
+  Result := FRAM[NormalizeRAMPointer(TRAMPointer(S))];
+end;
+
+function TProcessor.NormalizeRAMPointer(ARAMPointer: TRAMPointer): TRAMPointer;
+begin
+  if not CanNormalize(ARAMPointer) then
+    raise Exception.CreateFmt('Cannot process RAM-Pointer: 0x.2x', [ARAMPointer]);
+  case ARAMPointer of
+  RAMStart .. RAMEnd:
+    Result := ARAMPointer;
+  RAMMappedStart .. RAMMappedEnd:
+    Result := ARAMPointer - RegisterBankOffset;
+  end;
+end;
+
+function TProcessor.CanNormalize(ARAMPointer: TRAMPointer): Boolean;
+begin
+  Result := (ARAMPointer < RAMEnd) or (ARAMPointer >= RegisterBankOffset) and (ARAMPointer < RAMMappedEnd);
+end;
+
+procedure TProcessor.LoadProgram(AFileData: TStrings);
 var
   Counter: Integer;
-  HelpLineNumber: Cardinal;
-  help: string;
+  Line: Cardinal;
+  Instruction: DWORD;
 begin
   for Counter := 0 to AFileData.Count - 1 do
   begin
     if not AFileData[Counter].StartsWith(' ') then
     begin
-      HelpLineNumber := StrToDWord('$' + AFileData[Counter].Substring(0, 4));
-      FProgamMemory[HelpLineNumber].Command := StrToDWord('$' + AFileData[Counter].Substring(5, 4)) ;
-      FProgamMemory[HelpLineNumber].Line := Counter + 1;
+      Line := StrToDWord('$' + AFileData[Counter].Substring(0, 4));
+      Instruction := StrToDWord('$' + AFileData[Counter].Substring(5, 4));
+      try
+        FProgramMem[Line].Instruction := TInstruction(Instruction);
+      except
+        on ERangeError do
+          raise Exception.CreateFmt('Unknown Instruction! 0x%.2h', [Instruction]);
+      end;
+      FProgramMem[Line].Line := Counter + 1;
     end;
   end;
+end;
+
+procedure TProcessor.Initialize;
+begin
+  FProgramCounter := 0;
+  FProgramCounterStackPos := 0;
+  FillByte(FProgramCounterStack, SizeOf(FProgramCounterStack), 0);
+  FillByte(FRAM, SizeOf(FRAM), 0);
+end;
+
+procedure TProcessor.ResetROM;
+begin
+  FillByte(FROM, SizeOf(FROM), 0);
+end;
+
+procedure TProcessor.DoStep;
+begin
+
 end;
 
 end.
