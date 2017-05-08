9b1bcf889fa5b8e5aad01f606bf61d5b528748cd
SchmittAndre <andre1502@gmx.de>
2017-05-08 15:16:23 +0200

    added Help Menu

--- Source/Main.pas
+++ Source/Main.pas
@@ -20,6 +20,7 @@ type
   TfrmMain = class(TForm)
     actExit: TAction;
     actCompile: TAction;
+    actHelp: TAction;
     actNew: TAction;
     actSaveFileAs: TAction;
     actStepOut: TAction;
@@ -65,6 +66,8 @@ type
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
     MenuItem4: TMenuItem;
+    miHelp: TMenuItem;
+    miHelpHeader: TMenuItem;
     miNew: TMenuItem;
     miFileSplitter1: TMenuItem;
     miCompile: TMenuItem;
@@ -106,6 +109,7 @@ type
     procedure actCompileExecute(Sender: TObject);
     procedure actCompileUpdate(Sender: TObject);
     procedure actExitExecute(Sender: TObject);
+    procedure actHelpExecute(Sender: TObject);
     procedure actHideAllExecute(Sender: TObject);
     procedure actOpenFileExecute(Sender: TObject);
     procedure actOpenFileUpdate(Sender: TObject);
@@ -271,6 +275,11 @@ end;
 procedure TfrmMain.actExitExecute(Sender: TObject);
 begin
   Close;
+end;
+
+procedure TfrmMain.actHelpExecute(Sender: TObject);
+begin
+   raise ENotImplemented.Create('Help is not Implemented');
 end;
 
 procedure TfrmMain.actCompileExecute(Sender: TObject);
